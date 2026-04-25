475b62a5fad888d3c68e84fe36f88e90b5ffbda8
Gagarinov-ops <gagarinoff.one@yandex.ru>
2026-04-25 12:12:28 +0700

    Initial commit: test project for PDF font fix

---
+++ js/contract.js
@@ -0,0 +1,21 @@
+// js/contract.js
+async function generateContractPDF(data) {
+    const { jsPDF } = window.jspdf;
+    const doc = new jsPDF({
+        unit: 'mm',
+        format: 'a4',
+        orientation: 'portrait'
+    });
+    
+    // ПРОБЛЕМА ЗДЕСЬ: шрифт не подключается
+    doc.setFont("DejaVuSans");
+    
+    doc.setFontSize(16);
+    doc.text("ДОГОВОР № " + data.contractNumber, 105, 25, { align: 'center' });
+    
+    doc.setFontSize(11);
+    doc.text("г. " + data.city + " «25.04.2026» 2026 г.", 20, 40);
+    doc.text(data.customerFullname + ", именуемый Заказчиком, и " + data.executorFullname + ", именуемый Исполнителем...", 20, 55);
+    
+    doc.save(`Договор_${data.contractNumber}.pdf`);
+}
